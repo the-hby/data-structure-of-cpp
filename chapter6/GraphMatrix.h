bb9c9a557a7093043f50118fe4474693df3da906
hby <3206391486@qq.com>
2023-09-18 19:52:31 +0800

    fifteenth commit

--- chapter6/GraphMatrix.h
+++ chapter6/GraphMatrix.h
@@ -157,4 +157,130 @@ void Graph<Tv,Te>::DFS(int v,int&clock)
         status(v)=VISITED;fTime(v)=++clock;
     }
 }
+template<typename Tv,typename Te>
+Stack<Tv>* Graph<Tv,Te>::tSort(int s)
+{
+    reset();int clock=0;int v=s;
+    Stack<Tv>*S=new Stack<Tv>;
+    do {
+        if(UNDETERMINED==status(v))
+        {
+            if(!TSort(v,clock,S))
+            {
+                while(!S->empty())
+                {
+                    S->pop();break;
+                }
+            }
+        }
+    }while (s!=(v=(++v%n)));
+    return S;
+}
+template<typename Tv,typename Te>
+bool Graph<Tv,Te>::TSort(int v,int &clock,Stack<Tv>*S)
+{
+    dTime(v)=++clock;status(v)=DISCOVERED;
+    for(int u=firstNbr(v);u>-1;u=nextNbr(v,u))
+    {
+        switch(status(u))
+        {
+            case UNDICOVERED:
+            {
+                parent(u)=v;
+                type(v,u)=TREE;
+                if(!TSort(u,clock,S))
+                {
+                    return false;
+                }
+                break;
+            }
+            case DISCOVERED:type(v,u)=BACKWARD;return false;
+            default:type(v,u)=(dTime(v)<dTime(u))?FORWARD:CROSS;break;
+        }
+    }
+    status(v)=VISITED;S->push(vertex(v));return true;
+}
+template<typename Tv,typename Te>
+void Graph<Tv,Te>::bcc(int s)
+{
+    reset();int v=s;Stack<int>S;
+    do
+    {
+        if(UNDICOVERED==status(v))
+        {
+            BCC(v,clock,S);
+            S.pop();
+        }
+    }while(s!=(v=(++v%n)));
+}
+#define hca(x)(fTime(x))
+template<typename Tv,typename Te>
+void Graph<Tv,Te>::BCC(int v,int &clock,Stack<int>&S)
+{
+    hca(v)=dTime(v)=++clock;status(v)=DISCOVERED;
+    S.push(v);
+    for(int u=firstNbr(v);u>-1;u=nextNbr(v,u))
+    {
+        switch(status(u))
+        {
+            case UNDICOVERED:{
+                parent(u)=v;type(v,u)=TREE;BCC(u,clock,S);
+                if(hca(u)<dTime(v))
+                {
+                    hca(v)=std::min(hca(v),hca(u));
+                }
+                else {
+                    while(v!=S.pop())
+                    {
+                        S.push(v);
+                    }
+                }
+                break;
+            }
+            case DISCOVERED:
+            {
+                type(v,u)=BACKWARD;
+                if(u!=parent(v)){hca(v)=std::min(hca(v),dTime(u));}
+                break;
+            }
+            default:type(v,u)=(dTime(v)<dTime(u))?FORWARD:CROSS;break;
+        }
+    }
+    status(v)=VISITED;
+}
+template<typename Tv,typename Te>template<typename PU>
+void Graph<Tv,Te>::pfs(int s,PU prioUpdater)
+{
+    reset();int v=s;
+    do {
+        if(status(v)==UNDICOVERED)
+        {
+            PFS(v,prioUpdater);
+        }
+    }while (s==(v=(++v%n)));
+}
+template<typename Tv,typename Te>template<typename PU>
+void Graph<Tv,Te>::PFS(int s,PU prioUpdater)
+{
+    priority(s)=0;status(s)=VISITED;parent(s)=-1;
+    while(1)
+    {
+        for(int u=firstNbr(s);u>-1;u=nextNbr(s,u))
+        {
+            prioUpdater(this,s,u);
+        }
+        for(int shortest=INT32_MAX,u=0;u<n;u++)
+        {
+            if(UNDICOVERED==status(u))
+            {
+                if(shortest>priority(u))
+                {
+                    shortest=priority(u);s=u;
+                }
+            }
+        }
+        if(VISITED==status(s)){break;}
+        status(s)=VISITED;type(parent(s),s)=TREE;
+    }
+}
 #endif
